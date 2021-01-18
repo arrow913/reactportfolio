b1d631fd2f9444efcdd4bcd310e97ff46ab3444e
william <billy.arrasmith@gmail.com>
2021-01-17 21:20:06 -0600

    commiting portfolio

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# reactportfolio
+# Reactportfolio
---
+++ src/resumeData.js
@@ -0,0 +1,95 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/arrow913",
+    "name": "William Arrasmith",
+    "role": "Full Stack Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to learn about new technologies, code microprocessors, and design on 3D printer.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/wwe-show-2-a990621b3/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/arrow913",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"",
+          "url":"",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"",
+    "address":"Tenneesse",
+    "website":"https://github.com/arrow913",
+    "education":[
+      {
+        "UniversityName":"The Insitute of Vanderbuilt Boot Camp",
+        "specialization":"Full Stack Development",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2021",
+        "Achievements":"Certificate"
+      },
+      {
+        "UniversityName":"Nashville Auto Diesel College",
+        "specialization":"Automotive Repair",
+        "MonthOfPassing":"Sept",
+        "YearOfPassing":"2000",
+        "Achievements":"ASE Certifications"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Drews Total Auto Care",
+        "specialization":"Diagnostics and Repair on Automobiles",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2021",
+      },
+      {
+        "CompanyName":"Arrasmith Custom Automotive",
+        "specialization":"Creating Custom Cars",
+        "MonthOfLeaving":"Feb",
+        "YearOfLeaving":"2018",
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"SoundFinder",
+        "imgurl":"/images/Screen Shot 2020-09-21 at 9.16.35 AM.png"
+      },
+      {
+        "name":"project2",
+        "description":"HearthStone",
+        "imgurl":"/images/HearthstoneApp.png"
+      },
+      {
+        "name":"prodject 3",
+        "description":"LightFinder",  
+        "imgurl":"/images/coming soon.jpeg"
+      },
+      {
+        "name":"project4",
+        "description":"",
+        "imgurl":"/images/coming soon.jpeg"
+      }
+    ],
+    
+  }
+  
+  export default resumeData
