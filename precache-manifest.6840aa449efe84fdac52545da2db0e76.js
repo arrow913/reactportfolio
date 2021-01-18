2219910e8621746ec2988f161ada6654c24a5416
william <billy.arrasmith@gmail.com>
2021-01-17 22:02:14 -0600

    Updates

---
+++ precache-manifest.6840aa449efe84fdac52545da2db0e76.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78621e16234c71de7414",
+    "url": "/reactportfolio/static/css/main.1a2a57ff.chunk.css"
+  },
+  {
+    "revision": "78621e16234c71de7414",
+    "url": "/reactportfolio/static/js/main.78621e16.chunk.js"
+  },
+  {
+    "revision": "1048638a4991ffad96d3",
+    "url": "/reactportfolio/static/js/1.1048638a.chunk.js"
+  },
+  {
+    "revision": "f3444e3a511a2aa26237",
+    "url": "/reactportfolio/static/js/runtime~main.f3444e3a.js"
+  },
+  {
+    "revision": "f3a757a97483042dbd95d45fd36c0c2b",
+    "url": "/reactportfolio/index.html"
+  }
+];
